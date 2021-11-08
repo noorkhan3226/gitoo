cbb3dbf8dc05a05e6536680abc7ee51168bdab92
noorkhan3226 <noorafghan3226@gmail.com>
2021-11-08 14:11:31 -0500

    first Desk

---
+++ src/com/company/Desk.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Desk {
+
+    String color;
+    String type;
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Desk desk=new Desk();
+        desk.color="brown";
+        desk.type="Office";
+    }
+}
